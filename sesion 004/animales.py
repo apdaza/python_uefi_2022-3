920d990ea64beffcd1ada81311b4a558352865da
apdaza <apdaza@gmail.com>
2022-12-14 19:47:19 -0500

    modificado ejemplo animal

--- sesion 004/animales.py
+++ sesion 004/animales.py
@@ -1,3 +1,5 @@
+from random import choice
+
 class Animal:
     def __init__(self):
         self.nombre = ""
@@ -5,8 +7,40 @@ class Animal:
     def informar_tipo(self):
         print(type(self))
 
+    def comunicar(self):
+        pass
 
-gato = Animal()
-gato.nombre = "misifu"
-gato.informar_tipo()
-print(gato.nombre)
+class Gato(Animal):
+    def comunicar(self):
+        print("miauuuu")
+
+class Perro(Animal):
+    def comunicar(self):
+        print("guau")
+
+class Cacatua(Animal):
+    def comunicar(self):
+        print("pia-rua")
+
+class Boa(Animal):
+    def comunicar(self):
+        print("ssssssss")
+
+class Jirafa(Animal):
+    pass
+
+class TiendaMascotas:
+    def __init__(self):
+        self.animal = None
+        self.animales = [Gato(), Perro(), Cacatua(), Boa(), Jirafa()]
+
+    def seleccionar_animal(self):
+        self.animal = choice(self.animales)
+
+if __name__ == "__main__":
+    t = TiendaMascotas()
+    t.seleccionar_animal()
+    t.animal.informar_tipo()
+    t.animal.nombre = choice(['misi','laika','max','perry'])
+    print(t.animal.nombre)
+    t.animal.comunicar()

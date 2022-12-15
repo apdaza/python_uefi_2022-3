eb1a5f48a11ba8a78a12dbb0d23ae76ac7a3c7e0
apdaza <apdaza@gmail.com>
2022-12-14 21:02:54 -0500

    modificado la banda

--- sesion 004/banda/principal.py
+++ sesion 004/banda/principal.py
@@ -1,7 +1,10 @@
 from banda import *
+from random import choices, randint
 
-i = Banda([Guitarra(), Bajo(), Tiple(), Flauta(), Piano()])
-i.armar_banda(['Alejandro', 'Miguel', 'Juan', 'Maria'])
+instrumentos = choices([Guitarra(), Bajo(), Tiple(), Flauta(), Piano()], k=randint(1,5))
+nombres = choices(['Alejandro', 'Miguel', 'Juan', 'Maria'], k=randint(1,4))
+i = Banda(instrumentos)
+i.armar_banda(nombres)
 i.presentar()
 i.afinar()
 i.tocar()

3051e1bbb4e73108c12920652097afa6c249b9d0
apdaza <apdaza@gmail.com>
2022-12-19 20:56:40 -0500

    agregado ejemplo crono con tkinter

---
+++ sesion 006/cronometro/app_cronometro.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from cronometro import Cronometro
+from threading import *
+from time import sleep
+
+class AppCronometro(Thread):
+
+    def __init__(self):
+        self.principal = Tk()
+        self.crono = Cronometro()
+        self.frame = Frame(self.principal)
+        self.frame.pack()
+
+        self.valor = StringVar()
+        self.display = Label(self.frame, textvariable=self.valor,
+                             font=('Helvetica', 20))
+        self.display.pack(side=TOP)
+
+        self.boton_iniciar = Button(self.frame, text='Iniciar/Detener',
+                                    command=self.cambiar_estado)
+        self.boton_iniciar.pack(side=LEFT)
+
+        self.boton_borrar = Button(self.frame, text='Borrar',
+                                    command=self.borrar_cronometro)
+        self.boton_borrar.pack(side=RIGHT)
+
+        Thread.__init__(self)
+        self.start()
+
+        self.principal.mainloop()
+
+
+    def cambiar_estado(self):
+        self.crono.cambiar_estado()
+
+    def borrar_cronometro(self):
+        self.crono.borrar()
+
+    def run(self):
+        while True:
+            if not self.crono.parado:
+                self.crono.avanzar()
+                sleep(0.1)
+            self.valor.set(self.crono.ver_tiempo())
+
+    def callback(self):
+        print("saliendo")
+        self.principal.quit()
+
+
+app = AppCronometro()
---
+++ sesion 006/cronometro/cronometro.py
@@ -0,0 +1,50 @@
+from unidad_tiempo import UnidadTiempo
+
+class Cronometro:
+    def __init__(self):
+        self.hora = UnidadTiempo('Hora', 23)
+        self.minuto = UnidadTiempo('Minuto', 59)
+        self.segundo = UnidadTiempo('Segundo', 59)
+        self.decima = UnidadTiempo("Decima", 9)
+
+        self.parado = True
+
+    def avanzar(self):
+        self.decima.avanzar()
+        if self.decima.valor == 0:
+            self.segundo.avanzar()
+            if self.segundo.valor == 0:
+                self.minuto.avanzar()
+                if self.minuto.valor == 0:
+                    self.hora.avanzar()
+
+
+    def retroceder(self):
+        self.decima.retroceder()
+        if self.decima.valor == self.segundo.tope:
+            self.segundo.retroceder()
+            if self.segundo.valor == self.minuto.tope:
+                self.minuto.retroceder()
+                if self.minuto.valor == self.minuto.tope:
+                    self.hora.retroceder()
+
+    def borrar(self):
+        self.decima.borrar()
+        self.segundo.borrar()
+        self.minuto.borrar()
+        self.hora.borrar()
+
+    def ver_tiempo(self):
+        return "%s : %s : %s : %s" % (self.hora.ver_valor(),
+                                 self.minuto.ver_valor(),
+                                 self.segundo.ver_valor(),
+                                 self.decima.ver_valor())
+
+    def setear(self, valores):
+        self.hora.setear(valores[0])
+        self.minuto.setear(valores[1])
+        self.segundo.setear(valores[2])
+        self.decima.setear(valores[3])
+
+    def cambiar_estado(self):
+        self.parado = not self.parado
